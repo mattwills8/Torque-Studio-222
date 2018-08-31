a0eb9ddc6eef6b713774be6a898ae13c909d3541
mattwills8 <matt_wills8@hotmail.co.uk>
2018-08-31 16:39:13 +0100

    add tablet and desktop footer styling

--- src/parts/shared/footer.php
+++ src/parts/shared/footer.php
@@ -2,11 +2,11 @@
 
   <div class="row footer-content-wrapper" >
 
-    <div class="col1 col3-tablet col4-desktop s222-footer-col" >
+    <div class="col1 col3-tablet col4-desktop s222-footer-col footer-copy-tablet" >
       <?php get_sidebar( 'footer-col-1' ); ?>
     </div>
 
-    <div class="col1 col3-tablet col4-desktop s222-footer-col body-white" >
+    <div class="col1 col3-tablet col4-desktop s222-footer-col body-white footer-copy-tablet" >
       <?php get_sidebar( 'footer-col-2' ); ?>
 
       <div class="show-tablet">
@@ -14,11 +14,11 @@
       </div>
     </div>
 
-    <div class="col1 col3-tablet col4-desktop hide-tablet s222-footer-col body-white" >
+    <div class="col1 col3-tablet col4-desktop hide-tablet s222-footer-col body-white footer-copy-tablet" >
       <?php get_template_part( 'sidebar-footer-col-3' ); ?>
     </div>
 
-    <div class="col1 col3-tablet col4-desktop s222-footer-col" >
+    <div class="col1 col3-tablet col4-desktop s222-footer-col footer-copy-tablet" >
       <?php get_sidebar( 'footer-col-4' ); ?>
     </div>
 
