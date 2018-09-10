df29dd9ae27885447777c3adafaf1a38abdfd546
mattwills8 <matt_wills8@hotmail.co.uk>
2018-09-10 14:27:26 +0100

    add mofifications to single template for project posts

--- src/single.php
+++ src/single.php
@@ -7,6 +7,16 @@
 ?>
 <?php
 
+$cats = get_the_category();
+
+$is_project = false;
+foreach ($cats as $cat) {
+  if ($cat->slug === 'projects') {
+    $is_project = true;
+  }
+}
+
+
 get_template_part( 'parts/shared/html-header' );
 
 if ( have_posts() ) while ( have_posts() ) : the_post();
@@ -21,9 +31,19 @@ if ( have_posts() ) while ( have_posts() ) : the_post();
 
   		<?php get_template_part( 'parts/templates/content', 'single' ); ?>
 
-      <a href="/blog">
-        <button>View All Posts</button>
-      </a>
+
+      <?php if ($is_project) { ?>
+        <a href="/projects">
+          <button>View All Projects</button>
+        </a>
+
+      <?php } else { ?>
+        <a href="/blog">
+          <button>View All Posts</button>
+        </a>
+      <?php }?>
+
+
 
   </main>
 
