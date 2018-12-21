6de019489c57c90f262dfe89ead0475e4138fab7
mattwills8 <matt_wills8@hotmail.co.uk>
2018-12-21 16:53:01 +0000

    add location acf field

--- src/includes/acf/studio-222-child-acf-class.php
+++ src/includes/acf/studio-222-child-acf-class.php
@@ -20,6 +20,54 @@ class S222_ACF {
     if( function_exists('acf_add_local_field_group') ):
 
       acf_add_local_field_group(array(
+      	'key' => 'group_5c1cf2af67f4d',
+      	'title' => 'Project Fields',
+      	'fields' => array(
+      		array(
+      			'key' => 'field_5c1cf2c76c618',
+      			'label' => 'Location',
+      			'name' => 'location',
+      			'type' => 'text',
+      			'instructions' => '',
+      			'required' => 0,
+      			'conditional_logic' => 0,
+      			'wrapper' => array(
+      				'width' => '',
+      				'class' => '',
+      				'id' => '',
+      			),
+      			'default_value' => '',
+      			'placeholder' => '',
+      			'prepend' => '',
+      			'append' => '',
+      			'maxlength' => '',
+      		),
+      	),
+      	'location' => array(
+      		array(
+      			array(
+      				'param' => 'post_type',
+      				'operator' => '==',
+      				'value' => 'post',
+      			),
+      			array(
+      				'param' => 'post_taxonomy',
+      				'operator' => '==',
+      				'value' => 'category:projects',
+      			),
+      		),
+      	),
+      	'menu_order' => 0,
+      	'position' => 'acf_after_title',
+      	'style' => 'default',
+      	'label_placement' => 'top',
+      	'instruction_placement' => 'label',
+      	'hide_on_screen' => '',
+      	'active' => 1,
+      	'description' => '',
+      ));
+
+      acf_add_local_field_group(array(
       	'key' => 'group_5b8fc498c9251',
       	'title' => 'Page Footer',
       	'fields' => array(
